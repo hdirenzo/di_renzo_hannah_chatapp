ce53cfa35442639e803b9aba7e01a13e305dde3c
Hannah Di Renzo <hdirenzo@outlook.com>
2023-02-13 20:24:10 -0500

    Writing Notes

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+// Load Express library (Node.js web mini framework)
 const express = require('express');
+
+// Create the Expresss (web) application
 const app = express();
 
+// Load Node.js http library
 const http = require('http');
+
+// Create a new web server to host our app
 const server = http.createServer(app);
 
+// Load socket.io library
 const { Server } = require("socket.io");
+
+// Create a socket.io server on our application's web server
 const io = new Server(server);
 
 const port = process.env.PORT || 3000;
@@ -14,9 +23,10 @@ app.use(express.static('public'));
 
 // app.get is a route handler
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+// this next line actually starts up the web server and listens on the specified port
 server.listen(port, () => {
   console.log(`listening on ${port}`);
 });
@@ -24,11 +34,13 @@ server.listen(port, () => {
 // socket.io stuff goes here
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  // Send a message back to the client with the data
   socket.emit('connected', { sID: socket.id, message: 'new connection' });
 
   // listen for incoming messages from ANYone connected to the chat service
   // and then see what that message is
-  socket.on('chat_message', function(msg) { // step one - receive the message
+  socket.on('chat_message', function (msg) { // step one - receive the message
     console.log(msg);
 
     // step 2 - show everyone what was just sent through (send the message to everyone connected to the service)
@@ -36,7 +48,7 @@ io.on('connection', (socket) => {
   })
 
   //  listen for a typing event and broadcast to all
-  socket.on('user_typing', function(user) {
+  socket.on('user_typing', function (user) {
     console.log(user);
 
     io.emit('typing', { currentlytyping: user });
